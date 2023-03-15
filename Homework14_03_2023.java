d4253dee2f4edc784c2bf58a1d90b97b505182c3
vladlena-r <126571345+vladlena-r@users.noreply.github.com>
2023-03-15 16:14:55 +0100

    Homework14_03_2023

---
+++ Homework14_03_2023.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class Homework14_03_2023 {
+    public static void main(String[] args) {
+        String A = "Hello, Java! It's a Java Basic course";
+
+        System.out.println(A.length());
+        System.out.println(A.charAt(11));
+        System.out.println(A.substring(13,24));
+        System.out.println(A.substring(20));
+    }
+}
