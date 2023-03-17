5a5ad22eaff298bc4f5f8d2b6fd3f80b890adfe6
vladlena-r <126571345+vladlena-r@users.noreply.github.com>
2023-03-17 16:50:46 +0100

    Homework15.03
    2 classe create: CreateBox and Box

---
+++ Box.java
@@ -0,0 +1,29 @@
+package Homework;
+
+public class Box {
+    public static void main(String[] args) {
+
+        CreateBox Box = new CreateBox(49,40,30);
+
+        System.out.println();
+
+        CreateBox Box1 = new CreateBox(49,40);
+
+        System.out.println();
+
+        CreateBox Box2 = new CreateBox(30);
+
+        System.out.println();
+
+        CreateBox Box3 = new CreateBox();
+
+
+
+
+
+
+
+    }
+
+
+}
